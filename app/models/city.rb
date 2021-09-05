5ded6669fc5cef69af81fe0c4e883794efa5b677
dotnotation <dotinmotionproductions@gmail.com>
2021-09-05 17:40:36 -0400

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,12 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
+  include Reservable
+
+  def city_openings(start_date, end_date)
+    # return all of the listing objects that are available in the entire span inputted. 
+    openings(start_date, end_date)
+  end
+
 end
 
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,8 +1,48 @@
 class Listing < ActiveRecord::Base
-  belongs_to :neighborhood
+  belongs_to :neighborhood, required: true
   belongs_to :host, :class_name => "User"
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
+
+  validates :title, presence: true
+  validates :description, presence: true 
+  validates :address, presence: true
+  validates :listing_type, presence: true
+  validates :price, presence: true
+
+  before_save :make_host
+  before_destroy :host_status
+
+  def average_review_rating
+    reviews.average(:rating)
+  end
+
+  private
+
+  def self.available(start_date, end_date)
+    if start_date && end_date
+      joins(:reservations).
+        where.not(reservations: {checkin: start_date..end_date}) &
+      joins(:reservations).
+        where.not(reservations: {checkout: start_date..end_date})
+    else
+      []
+    end
+  end
+
+  # Makes user a host when a listing is created
+  def make_host
+    unless self.host.host
+      self.host.update(:host => true)
+    end
+  end
+
+  # Changes host status to false when listing is destroyed and user has no more listings
+  def host_status
+    if self.host.listings.count <= 1
+      self.host.update(:host => false)
+    end
+  end
   
 end
